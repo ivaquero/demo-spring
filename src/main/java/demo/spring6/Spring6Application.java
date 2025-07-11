b3c646c899a5d20ba4d836398b28e1038fc92c99
ivaquero <msaintms@outlook.com>
2025-07-11 11:11:19 +0800

    demo: update project info

--- src/main/java/demo/spring6/Spring6Application.java
+++ src/main/java/demo/spring6/Spring6Application.java
@@ -1,4 +1,4 @@
-package demo.web;
+package demo.spring6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoSpringApplication {
+public class Spring6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringApplication.class, args);
+        SpringApplication.run(Spring6Application.class, args);
     }
 
     @GetMapping("/hello")
--- src/test/java/demo/spring6/Spring6ApplicationTests.java
+++ src/test/java/demo/spring6/Spring6ApplicationTests.java
@@ -1,10 +1,10 @@
-package demo.web;
+package demo.spring6;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoSpringApplicationTests {
+class Spring6ApplicationTests {
 
     @Test
     void contextLoads() {
